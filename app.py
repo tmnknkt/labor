6e4f2211f6bf8dbc4cef42c3e34025c8ef797661
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-09-26 03:22:53 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def not_found(err):
     return "Нет такой страницы", 404
 
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html>
         <html>
@@ -20,7 +20,7 @@ def web():
             'Content-Type': 'text/plain; charset=utf-8'
             }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Атаманкина Екатерина Романовна"
     group = "ФБИ-33"
@@ -32,11 +32,11 @@ def author():
                 <p>Студент: """ + name + """</p>
                 <p>Группа: """ + group + """</p>
                 <p>Факультет: """ + faculty + """</p>
-                <a href="/web">web</a>
+                <a href="/lab1/web">web</a>
             </body>
         </html>"""
 
-@app.route("/image")
+@app.route("/lab1/image")
 def image():
     path = url_for("static", filename="oak.jpg")
     css_path = url_for("static", filename="lab1.css") 
@@ -55,7 +55,7 @@ def image():
 
 count = 0
 
-@app.route('/counter')
+@app.route('/lab1/counter')
 def counter():
     global count
     count += 1
@@ -79,17 +79,17 @@ def counter():
 </html>
 '''
 
-@app.route('/clear_counter')
+@app.route('/lab1/clear_counter')
 def clear_counter():
     global count
     count = 0
     return redirect(url_for('counter'))
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
-@app.route("/created")
+@app.route("/lab1/created")
 def created():
     return '''
 <!doctype html>
