129245ef33f6eb219494546caccf85fb9bfa7a53
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-10-10 18:00:00 +0700

    Имя студента

--- app.py
+++ app.py
@@ -448,4 +448,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Атаманкина Екатерина'
+    return render_template('example.html', name=name)
