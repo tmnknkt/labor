6447dbd3ad953390dba8182710991e6ebfc4580a
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-10-23 20:58:51 +0700

    Валидаций

--- lab3.py
+++ lab3.py
@@ -28,7 +28,10 @@ def del_cookie():
 
 @lab3.route('/lab3/form1')
 def form1():
+    errors = {}
     user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
     age = request.args.get('age')
     sex = request.args.get('sex')
-    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
