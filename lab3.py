1c8f8764b5196801b9b596e8aa610bf0d94e22bc
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-10-24 13:15:50 +0700

    Аноним, вывод возраста

--- lab3.py
+++ lab3.py
@@ -6,7 +6,8 @@ lab3 = Blueprint('lab3', __name__)
 def lab3_3():
     name = request.cookies.get('name')
     name_color = request.cookies.get('name_color')
-    return render_template('lab3/lab3.html', name=name, name_color=name_color)
+    age = request.cookies.get('age')
+    return render_template('lab3/lab3.html', name=name, name_color=name_color, age=age)
 
 
 @lab3.route('/lab3/cookie/')
