5f10d0b711ecc085973070677287a86579bdca88
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-11-07 20:35:19 +0700

    Список пользователей

--- lab4.py
+++ lab4.py
@@ -124,6 +124,15 @@ def tree():
     return redirect('/lab4/tree')
 
 
+
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'katya', 'password': '177'},
+    {'login': 'pop', 'password': '111'},
+]
+
+
 @lab4.route('/lab4/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -132,8 +141,9 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('/lab4/login.html', login=login, authorized=True)
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('/lab4/login.html', login=login, authorized=True)
 
     if login == 'alex' and password == '123':
         return render_template('lab4/login.html', error='Успешная авторизация')
