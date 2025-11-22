a71c83e12d929fb848279e1eaf815e400b3296b7
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-11-22 09:48:49 +0700

    Регистрация

--- lab5.py
+++ lab5.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+import psycopg2
 lab5 = Blueprint('lab5', __name__)
 
 
@@ -13,9 +14,38 @@ def login():
     return "Страница входа"
 
 
-@lab5.route('/lab5/register')
+@lab5.route('/lab5/register', methods = ['GET', 'POST'])
 def register():
-    return "Страница регистрации"
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+
+
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login or password):
+        return render_template('lab5/register.html', error='Заполните все поля')
+
+    conn = psycopg2.connect(
+        host = 'localhost',
+        database = 'atamankina_knowledge_base',
+        user = 'tmnknkt',
+        password = '111'
+    )
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    if cur.fetchone():
+        cur.close()
+        conn.close()
+        return render_template('lab5/register.html',
+                               error="Такой пользователь уже существует")
+    
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return render_template('lab5/success.html', login=login)
 
 
 @lab5.route('/lab5/list')
