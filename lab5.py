e697123929bcc691bec0c2af2baad93286ab60ad
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-11-27 22:31:37 +0700

    Удаление дублирующегося кода

--- lab5.py
+++ lab5.py
@@ -10,6 +10,24 @@ def lab():
     return render_template('lab5/lab5.html', login=session.get('login'))
 
 
+def db_connect():
+    conn = psycopg2.connect(
+        host = 'localhost',
+        database = 'atamankina_knowledge_base',
+        user = 'tmnknkt',
+        password = '111'
+    )
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    return conn, cur
+
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+
 @lab5.route('/lab5/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -21,12 +39,7 @@ def login():
     if not (login or password):
         return render_template('lab5/login.html', error="Заполните поля")
     
-    conn = psycopg2.connect(
-        host = 'localhost',
-        database = 'atamankina_knowledge_base',
-        user = 'tmnknkt',
-        password = '111'
-    )
+    conn, cur = db_connect()
 
     cur = conn.cursor(cursor_factory = RealDictCursor)
 
@@ -34,18 +47,15 @@ def login():
     user = cur.fetchone()
 
     if not user:
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
 
     if user['password'] != password:
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
 
     session['login'] = login
-    cur.close()
-    conn.close()
+    db_close(conn, cur)
     return render_template('lab5/success_login.html', login=login)
 
 
@@ -61,25 +71,16 @@ def register():
     if not (login or password):
         return render_template('lab5/register.html', error='Заполните все поля')
 
-    conn = psycopg2.connect(
-        host = 'localhost',
-        database = 'atamankina_knowledge_base',
-        user = 'tmnknkt',
-        password = '111'
-    )
-    cur = conn.cursor()
+    conn, cur = db_connect()
 
     cur.execute(f"SELECT login FROM users WHERE login='{login}';")
     if cur.fetchone():
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/register.html',
                                error="Такой пользователь уже существует")
     
     cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
-    conn.commit()
-    cur.close()
-    conn.close()
+    db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
 
 
