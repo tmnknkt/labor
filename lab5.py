648337c33041f6d9fb63593bc66bdaa1ea711bf7
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-11-28 00:00:37 +0700

    Вывод статей

--- lab5.py
+++ lab5.py
@@ -89,8 +89,21 @@ def register():
 
 
 @lab5.route('/lab5/list')
-def list_articles():
-    return "Список статей"
+def list():
+    login = session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+    
+    conn, cur = db_connect()
+
+    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
+    login_id = cur.fetchone()["id"]
+
+    cur.execute(f"SELECT * FROM articles WHERE user_id='{login_id}';")
+    articles = cur.fetchall()
+
+    db_close(conn, cur)
+    return render_template('/lab5/articles.html', articles=articles)
 
 
 @lab5.route('/lab5/create', methods = ['GET', 'POST'])
