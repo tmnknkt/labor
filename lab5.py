edee066167f268bfe570e93c37242fa1e2567431
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-11-27 22:21:29 +0700

    Аутентификация

--- lab5.py
+++ lab5.py
@@ -1,17 +1,52 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session
 import psycopg2
+from psycopg2.extras import RealDictCursor
 lab5 = Blueprint('lab5', __name__)
 
 
 @lab5.route('/lab5/')
 def lab():
-    name = request.cookies.get('name')
-    return render_template('lab5/lab5.html', name=name)
+    login = request.form.get('login')
+    return render_template('lab5/lab5.html', login=session.get('login'))
 
 
-@lab5.route('/lab5/login')
+@lab5.route('/lab5/login', methods = ['GET', 'POST'])
 def login():
-    return "Страница входа"
+    if request.method == 'GET':
+        return render_template('lab5/login.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+    
+    if not (login or password):
+        return render_template('lab5/login.html', error="Заполните поля")
+    
+    conn = psycopg2.connect(
+        host = 'localhost',
+        database = 'atamankina_knowledge_base',
+        user = 'tmnknkt',
+        password = '111'
+    )
+
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+
+    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    user = cur.fetchone()
+
+    if not user:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+
+    if user['password'] != password:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+
+    session['login'] = login
+    cur.close()
+    conn.close()
+    return render_template('lab5/success_login.html', login=login)
 
 
 @lab5.route('/lab5/register', methods = ['GET', 'POST'])
@@ -46,6 +81,7 @@ def register():
     cur.close()
     conn.close()
     return render_template('lab5/success.html', login=login)
+
 
 
 @lab5.route('/lab5/list')
