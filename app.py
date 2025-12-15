f27622920ef3e1ce81e4bada600b9f903f609f5a
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-12-15 12:58:58 +0700

    Заготовка к 7-ой лабораторной

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 import datetime
 import os
 
@@ -21,6 +22,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 
 # Глобальная переменная для хранения лога (в реальном приложении лучше использовать БД)
@@ -165,6 +167,7 @@ def index():
             <a href="/lab4">Четвертая лабораторная</a><br>
             <a href="/lab5">Пятая лабораторная</a><br>
             <a href="/lab6">Шестая лабораторная</a><br>
+            <a href="/lab7">Седьмая лабораторная</a><br>
         </nav>
                 
         <footer>
