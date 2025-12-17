6269e962b380d605b0cf8bfc22a4d18d2d1aacd8
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-12-17 20:55:02 +0700

    Редактирование фильмов

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -24,7 +24,11 @@ function fillFilmList() {
             // Создаем кнопки действий
             let editButton = document.createElement('button');
             editButton.innerText = 'редактировать';
-            
+
+            editButton.onclick = function() {
+                editFilm(i);        
+            };
+
             let delButton = document.createElement('button');
             delButton.innerText = 'удалить';
 
@@ -85,14 +89,16 @@ function addFilm() {
 }
 
 function sendFilm() {
+    const id = document.getElementById('id').value;
     const film = {
         title: document.getElementById('title').value,
         title_ru: document.getElementById('title-ru').value,
         year: document.getElementById('year').value,
         description: document.getElementById('description').value
     }
-    const url = `/lab7/rest-api/films/`;
-    const method = 'POST';
+
+    const url = `/lab7/rest-api/films/${id}`;
+    const method = id === '' ? 'POST' : 'PUT';
 
     fetch(url, {
         method: method,
@@ -106,4 +112,18 @@ function sendFilm() {
 
 }
 
+function editFilm(id) {
+  fetch(`/lab7/rest-api/films/${id}`)
+    .then(function(data) {
+      return data.json();
+    })
+    .then(function(film) {
+      document.getElementById('id').value = id;
+      document.getElementById('title').value = film.title;
+      document.getElementById('title-ru').value = film.title_ru;
+      document.getElementById('year').value = film.year;
+      document.getElementById('description').value = film.description;
+      showModal();
+    });
+}
 
