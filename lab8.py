fa11e4175ccfb4919d3068c5e5403a8960bb2c26
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-12-29 15:58:02 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -4,6 +4,9 @@ from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 from os import path
+from db import db
+from db.models import users, articles
+
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -18,9 +21,33 @@ def login():
     return "Страница входа"
 
 
-@lab8.route('/lab8/register')
+@lab8.route('/lab8/register/', methods=['GET', 'POST'])
 def register():
-    return "Страница регистрации"
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form or login_form.strip() == '':
+        return render_template('lab8/register.html',
+                            error='Имя пользователя не может быть пустым')
+
+    if not password_form or password_form.strip() == '':
+        return render_template('lab8/register.html',
+                            error='Пароль не может быть пустым')
+
+    login_exists = users.query.filter_by(login=login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html',
+                            error='Такой пользователь уже существует')
+
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login=login_form, password=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return redirect('/lab8/')
 
 
 @lab8.route('/lab8/articles')
