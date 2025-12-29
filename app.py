b429cadbbeca3bdfb7c45cef9894f1e5c0126257
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-12-29 17:42:49 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 from rgz import rgz_bp
 import datetime
 import os
@@ -53,6 +54,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 app.register_blueprint(rgz_bp, url_prefix='/rgz')
 
 
@@ -200,6 +202,7 @@ def index():
             <a href="/lab6">Шестая лабораторная</a><br>
             <a href="/lab7">Седьмая лабораторная</a><br>
             <a href="/lab8">Восьмая лабораторная</a><br>
+            <a href="/lab9">Девятая лабораторная</a><br>
             <a href="/rgz">РГЗ (мебельный магазин)</a><br>
         </nav>
                 
