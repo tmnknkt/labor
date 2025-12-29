96de7671c8f0a51eee8bf71f80e088d887fb89d4
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-12-29 15:13:45 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, url_for, request, redirect, abort, render_template
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+from os import path
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -17,6 +20,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 
+if app.config['DB_TYPE'] == 'postgres':
+  db_name = 'atamankina_orm'
+  db_user = 'tmnknkt'
+  db_password = '111'
+  host_ip = '127.0.0.1'
+  host_port = 5432
+
+  app.config['SQLALCHEMY_DATABASE_URI'] = \
+      f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+  dir_path = path.dirname(path.realpath(__file__))
+  db_path = path.join(dir_path, "atamankina_orm.db")
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
