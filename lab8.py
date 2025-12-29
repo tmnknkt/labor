d45a78fabbb7ec7e57bbda16387e472baf5ddde9
Ekaterina Atamankina <atamankina.2005@gmail.com>
2025-12-29 16:44:27 +0700

    Самостоятельные задания

--- lab8.py
+++ lab8.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template, request, session, redirect, current_app, url_for
+from flask import Blueprint, render_template, request, session, redirect, current_app, url_for, flash
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 from os import path
 from db import db
-from db.models import users, articles
+from db.models import users, articles as ArticleModel
 from flask_login import login_user, login_required, current_user, logout_user
 
 
@@ -13,11 +13,7 @@ lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8/')
 def lab():
-    if current_user.is_authenticated:
-        return render_template('lab8/lab8.html', login=current_user.login)
-    else:
-        return render_template('lab8/lab8.html', login=None)
-
+    return render_template('lab8/lab8.html')
 
 @lab8.route('/lab8/login', methods=['GET', 'POST'])
 def login():
@@ -26,20 +22,19 @@ def login():
 
     login_form = request.form.get('login')
     password_form = request.form.get('password')
+    remember = request.form.get('remember') == 'on'
 
     if not login_form or login_form.strip() == '':
-        return render_template('lab8/login.html',
-                            error='Введите логин')
+        return render_template('lab8/login.html', error='Введите логин')
 
     if not password_form or password_form.strip() == '':
-        return render_template('lab8/login.html',
-                            error='Введите пароль')
+        return render_template('lab8/login.html', error='Введите пароль')
 
     user = users.query.filter_by(login=login_form).first()
 
     if user:
         if check_password_hash(user.password, password_form):
-            login_user(user, remember=False)
+            login_user(user, remember=remember)
             return redirect('/lab8/')
 
     return render_template('lab8/login.html',
@@ -72,18 +67,90 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     
+    login_user(new_user, remember=False)
     return redirect('/lab8/')
 
 
 @lab8.route('/lab8/articles')
 @login_required
 def articles():
-    return "Список статей"
+    user_articles = ArticleModel.query.filter_by(login_id=current_user.id).all()
+    return render_template('lab8/articles.html', articles=user_articles)
 
 
-@lab8.route('/lab8/create')
+@lab8.route('/lab8/create', methods=['GET', 'POST'])
+@login_required
 def create_article():
-    return "Создание статьи"
+    if request.method == 'GET':
+        return render_template('lab8/create.html')
+    
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+    is_favorite = request.form.get('is_favorite') == 'on'
+    is_public = request.form.get('is_public') == 'on'
+    
+    if not title or not article_text:
+        return render_template('lab8/create.html', 
+                             error='Заполните все обязательные поля')
+    
+    new_article = ArticleModel(
+        login_id=current_user.id,
+        title=title,
+        article_text=article_text,
+        is_favorite=is_favorite,
+        is_public=is_public,
+        likes=0
+    )
+    
+    db.session.add(new_article)
+    db.session.commit()
+    
+    return redirect('/lab8/articles')
+
+
+@lab8.route('/lab8/edit/<int:article_id>', methods=['GET', 'POST'])
+@login_required
+def edit_article(article_id):
+    article = ArticleModel.query.get_or_404(article_id)
+    
+    if article.login_id != current_user.id:
+        return redirect('/lab8/articles')
+    
+    if request.method == 'GET':
+        return render_template('lab8/edit.html', article=article)
+    
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+    is_favorite = request.form.get('is_favorite') == 'on'
+    is_public = request.form.get('is_public') == 'on'
+    
+    if not title or not article_text:
+        return render_template('lab8/edit.html', 
+                             article=article,
+                             error='Заполните все обязательные поля')
+    
+    article.title = title
+    article.article_text = article_text
+    article.is_favorite = is_favorite
+    article.is_public = is_public
+    
+    db.session.commit()
+    
+    return redirect('/lab8/articles')
+
+
+@lab8.route('/lab8/delete/<int:article_id>')
+@login_required
+def delete_article(article_id):
+    article = ArticleModel.query.get_or_404(article_id)
+    
+    if article.login_id != current_user.id:
+        return redirect('/lab8/articles')
+    
+    db.session.delete(article)
+    db.session.commit()
+    
+    return redirect('/lab8/articles')
 
 
 @lab8.route('/lab8/logout')
